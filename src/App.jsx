0b66c8b5210373e3ae021547c1bab26ff2a8d94b
Cidar de Alencar <101343945+CidardeAlencar@users.noreply.github.com>
2023-10-16 19:52:54 -0400

    otra version

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./navbar";
+import Navbar from "./components/navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpaceShuttle } from "@fortawesome/free-solid-svg-icons";
 import CountdownTimer from "./components/cuenta";
 import options from "./components/data";
 import Tarjeta from "./components/card";
 import { Row, Col } from "react-bootstrap";
+import perfil from "./assets/perfil.jpg"
 const App = () => {
   console.log(Row);
   return (
-    <div className="container">
+    <div className="container-fluid">
       <Navbar />
       <h3 className="planet-inner"></h3>
       <div className="mars-title">
@@ -28,17 +29,15 @@ const App = () => {
           <div className="name-list">
             <h1>Tripulantes</h1>
           
-            <ul className="column-list">
+            <ul className="row column-list">
               {options.map((option, index) => (
-                <li key={index}>
+                <li key={index} className="col-lg-3 col-md-4">
                   <div className="contenedor-tarjetas">
                     <Tarjeta
                       titulo={option.nombre}
                       descripcion={option.id}
-                      imagenURL="imagen1.jpg"
+                      imagenURL={perfil}
                     />
-                  
-                 
                   </div>
                 </li>
               ))}
--- src/components/data.js
+++ src/components/data.js
@@ -11,41 +11,41 @@ var options = [
     id: 3,
     nombre: "ERICK RAMOS QUISBERT",
   },
-//   {
-//     id: 4,
-//     nombre: "ERICK CRISTHIAN ZACARIAS TORREZ",
-//   },
-//   {
-//     id: 5,
-//     nombre: "DANIELSARABIA YUJRA",
-//   },
-//   {
-//     id: 6,
-//     nombre: "JAZMIN ROSA",
-//   },
-//   {
-//     id: 7,
-//     nombre: "DILAN APOLACA MARINO",
-//   },
-//   {
-//     id: 8,
-//     nombre: "DURAN AVENDA EDISON",
-//   },
-//   {
-//     id: 9,
-//     nombre: "ROSSIE JASHIEL GUTIEREZ SARDINAS",
-//   },
-//   {
-//     id: 10,
-//     nombre: "NAHOMI HUMEREZ",
-//   },
-//   {
-//     id: 11,
-//     nombre: "RUBEN QUISPE VALDA",
-//   },
-//   {
-//     id: 12,
-//     nombre: "EDMUNDO MERCADO",
-//   },
+  {
+    id: 4,
+    nombre: "ERICK CRISTHIAN ZACARIAS TORREZ",
+  },
+  {
+    id: 5,
+    nombre: "DANIELSARABIA YUJRA",
+  },
+  {
+    id: 6,
+    nombre: "JAZMIN ROSA",
+  },
+  {
+    id: 7,
+    nombre: "DILAN APOLACA MARINO",
+  },
+  {
+    id: 8,
+    nombre: "DURAN AVENDA EDISON",
+  },
+  {
+    id: 9,
+    nombre: "ROSSIE JASHIEL GUTIEREZ SARDINAS",
+  },
+  {
+    id: 10,
+    nombre: "NAHOMI HUMEREZ",
+  },
+  {
+    id: 11,
+    nombre: "RUBEN QUISPE VALDA",
+  },
+  {
+    id: 12,
+    nombre: "EDMUNDO MERCADO",
+  },
 ];
 export default options;
