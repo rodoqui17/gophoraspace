a1e08161952d6bbf5dbc6df0a6bf4213c8869e5f
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2023-10-16 17:20:16 -0400

    update 16102023

--- src/App.js
+++ src/App.js
@@ -1,50 +1,66 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./navbar";
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
+// import Navbar from "./navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpaceShuttle } from "@fortawesome/free-solid-svg-icons";
 import CountdownTimer from "./components/cuenta";
 import options from "./components/data";
 import Tarjeta from "./components/card";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container, Card } from "react-bootstrap";
 const App = () => {
   console.log(Row);
   return (
-    <div className="container">
-      <Navbar />
+    <div>
+      {/* <Navbar /> */}
       <h3 className="planet-inner"></h3>
       <div className="mars-title">
         <h1>#GOPHORALANZATUNOMBREALESPACIO</h1>
-        <h2 className="h2">OCTUBRE 2023</h2>
-        <h3 className="planet"></h3>
+        <h2 className="h2">11 NOVIEMBRE</h2>
 
+        <h3 className="planet"></h3>
         <br />
       </div>
 
       <CountdownTimer />
 
       <div className="mars-background">
-        <div>
-          <div className="name-list">
-            <h1>Tripulantes</h1>
-          
-            <ul className="column-list">
-              {options.map((option, index) => (
-                <li key={index}>
-                  <div className="contenedor-tarjetas">
-                    <Tarjeta
-                      titulo={option.nombre}
-                      descripcion={option.id}
-                      imagenURL="imagen1.jpg"
-                    />
-                  
-                 
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
+        <h1>Tripulantes</h1>
+        <Container>
+          <Row>
+            {options.map((option, index) => (
+              <Col xs={6} lg={3}>
+                <Card style={{ width: "auto", padding: "10px", margin: "4px" }}>
+                  <Card.Img variant="top" src={option.img} />
+                  <Card.Body>
+                    <Card.Title>{option.nombre}</Card.Title>
+                    <Card.Text>Tripulante # {option.id}</Card.Text>
+                    {/* <Button variant="primary">Go somewhere</Button> */}
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+
+        {/* <Container>
+          <Row>
+            <Col xs={6} lg={3}>
+              <Card style={{ width: "28rem" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container> */}
       </div>
     </div>
   );
--- src/components/data.js
+++ src/components/data.js
@@ -1,51 +1,65 @@
+import React from "react";
+import img from "../img/1.jpg"
 var options = [
   {
     id: 1,
     nombre: "LUIS PARICOLLO",
+    img: img
   },
   {
     id: 2,
     nombre: "IVAN HUMBERT",
+    img: img
   },
   {
     id: 3,
     nombre: "ERICK RAMOS QUISBERT",
+    img: img
   },
-//   {
-//     id: 4,
-//     nombre: "ERICK CRISTHIAN ZACARIAS TORREZ",
-//   },
-//   {
-//     id: 5,
-//     nombre: "DANIELSARABIA YUJRA",
-//   },
-//   {
-//     id: 6,
-//     nombre: "JAZMIN ROSA",
-//   },
-//   {
-//     id: 7,
-//     nombre: "DILAN APOLACA MARINO",
-//   },
-//   {
-//     id: 8,
-//     nombre: "DURAN AVENDA EDISON",
-//   },
-//   {
-//     id: 9,
-//     nombre: "ROSSIE JASHIEL GUTIEREZ SARDINAS",
-//   },
-//   {
-//     id: 10,
-//     nombre: "NAHOMI HUMEREZ",
-//   },
-//   {
-//     id: 11,
-//     nombre: "RUBEN QUISPE VALDA",
-//   },
-//   {
-//     id: 12,
-//     nombre: "EDMUNDO MERCADO",
-//   },
+  {
+    id: 4,
+    nombre: "ERICK CRISTHIAN ZACARIAS TORREZ",
+    img: img
+  },
+  {
+    id: 5,
+    nombre: "DANIELSARABIA YUJRA",
+    img: img
+  },
+  {
+    id: 6,
+    nombre: "JAZMIN ROSA",
+    img: img
+  },
+  {
+    id: 7,
+    nombre: "DILAN APOLACA MARINO",
+    img: img
+  },
+  {
+    id: 8,
+    nombre: "DURAN AVENDA EDISON",
+    img: img
+  },
+  {
+    id: 9,
+    nombre: "ROSSIE JASHIEL GUTIEREZ SARDINAS",
+    img: img
+  },
+  {
+    id: 10,
+    nombre: "NAHOMI HUMEREZ",
+    img: img
+  },
+  {
+    id: 11,
+    nombre: "RUBEN QUISPE VALDA",
+    img: img
+  },
+  {
+    id: 12,
+    nombre: "EDMUNDO MERCADO",
+    img: img
+  },
 ];
 export default options;
