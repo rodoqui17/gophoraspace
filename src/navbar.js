f669dd04513a9a699bc0dbea37f5c9ba3512a620
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2023-10-17 00:58:45 -0400

    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import './index.css'; // Importa el archivo CSS
+import '../index.css'; // Importa el archivo CSS
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><a href="/">Inicio</a></li>
-        <li><a href="/acerca">Acerca de</a></li>
-        <li><a href="/servicios">Servicios</a></li>
-        <li><a href="/contacto">Contacto</a></li>
-      </ul>
+    <nav class="navbar navbar-black navbar-expand-lg" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Gophora</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/acerca">Acerca de</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/servicios">Servicios</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contacto">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-  );
+  )
 };
 
 export default Navbar;
