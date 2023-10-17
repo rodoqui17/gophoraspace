b3625f9b864b27021f1b588deff6e6266308c129
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2023-10-17 00:52:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
-// import Navbar from "./navbar";
+import Navbar from "./navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpaceShuttle } from "@fortawesome/free-solid-svg-icons";
 import CountdownTimer from "./components/cuenta";
