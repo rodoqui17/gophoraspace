99d16fc1f3fd974cd912f49481cb3cd37bb7e3b8
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2023-10-18 02:14:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const App = () => {
                     {/* <Button variant="primary">Go somewhere</Button> */}
                   </Card.Body>
                 </Card>
-              <div/>
+              </div>
               </Col>
             ))}
           </Row>
