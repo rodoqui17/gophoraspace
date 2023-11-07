89d97afbf8945f1a0bf380dda416aa1af8aa3e5d
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2023-11-06 22:10:31 -0400

    update 06112023

---
+++ src/components/data_comando.js
@@ -0,0 +1,31 @@
+import React from "react";
+import img from "../img/astronauta.png"
+var datacomando = [
+  {
+    id: 1,
+    nombre: "ANDREA COVARRUBIAS",
+    img: "https://nftstorage.link/ipfs/bafybeihbmj5ymygbnkrtjmysn2ifbcuuykfoipcorfztwpvru7zm4ja2ki/acovarrubias.jpeg"
+  },
+  {
+    id: 2,
+    nombre: "CIDAR DE ALENCAR",
+    img: "https://nftstorage.link/ipfs/bafybeihbmj5ymygbnkrtjmysn2ifbcuuykfoipcorfztwpvru7zm4ja2ki/cdealencar.jpeg"
+  },
+  {
+    id: 3,
+    nombre: "RODRIGO QUIROZ",
+    img: img
+  },
+  {
+    id: 4,
+    nombre: "TOMAS PEÑALOZA",
+    img: img
+  },
+  {
+    id: 5,
+    nombre: "OSCAR PEREZ",
+    img: img
+  },
+  
+];
+export default datacomando;
